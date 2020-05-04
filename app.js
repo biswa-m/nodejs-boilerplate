3299dbef2bec7f0807b32d9cea129e13d0dedb30
biswa-m <bis.mhnta@gmail.com>
2020-05-04 05:54:47 +0530

    Set different path to serve staic files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var debug = require("debug")("mytooltruck-api:server");
+var debug = require("debug")("myapp-api:server");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -41,19 +41,17 @@ mongoose
     throw err;
   });
 
+var routes = require("./routes/index");
+
 var app = express();
+app.set("view engine", "ejs");
 
 app.use(logger(config.logger));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-
-app.set("view engine", "ejs");
-
-var routes = require("./routes/index");
 app.use("/api", routes);
-
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 module.exports = app;
